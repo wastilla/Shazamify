8c14105ea0b27b268277255ad9f39871df2c789e
wastilla <wastilla@email.unc.edu>
2022-11-10 17:49:02 -0500

    Added Xcode project

---
+++ Shazamify/ShazamifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamifyApp.swift
+//  Shazamify
+//
+//  Created by will astilla on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShazamifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
